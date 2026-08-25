16bfa1abdc40cca1cd74df060e4cf32c8445da98
Emanuel <emanuel.perez87@unach.mx>
2026-08-25 14:37:13 -0600

    Archivo Inicial

---
+++ MyNawal/MyNawal/MyNawalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNawalApp.swift
+//  MyNawal
+//
+//  Created by Emanuel on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MyNawalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
